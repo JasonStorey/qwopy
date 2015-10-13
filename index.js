f62b69f6250029f5070286ea7136c45549af0d33
Jason A. Storey <jason.storey@unrulymedia.com>
2015-10-13 16:25:04 +0100

    Add update button and textarea

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let dataDisplay = require('./lib/data-display');
+let dataDisplay = require('./lib/data-display'),
+    domTools = require('./lib/dom-tools');
 
 module.exports = {
     init: (config) => {
         console.log('QWOPY');
         dataDisplay.init(config);
+        let button = domTools.createElement('button');
+        button.innerHTML = 'update';
+        domTools.appendChild(button, config.container);
+        button.addEventListener('click', () => dataDisplay.updateQR());
     }
 };
 
